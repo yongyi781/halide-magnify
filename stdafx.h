2a25088820da14a2f0ead5e2f161e8c01fc4e0ea
Yongyi Chen <yongyi781@hotmail.com>
2014-03-11 23:38:44 -0400

    second commit

---
+++ stdafx.h
@@ -0,0 +1,18 @@
+#ifndef STDAFX_H_INCLUDED
+#define STDAFX_H_INCLUDED
+
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <chrono>
+#include <sstream>
+#include <Halide.h>
+#include <float.h>
+
+using Halide::Image;
+#include <image_io.h>
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#endif // STDAFX_H_INCLUDED
